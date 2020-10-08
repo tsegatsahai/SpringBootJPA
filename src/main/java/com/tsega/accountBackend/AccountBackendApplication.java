598e01f9db4edb37f7171ef272944b15ab96f246
Tsega Tsahai <tsegatsahai@Tsegas-MacBook-Air.local>
2020-10-08 15:47:31 -0500

    initialized springboot project

---
+++ src/main/java/com/tsega/accountBackend/AccountBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tsega.accountBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsega/accountBackend/AccountBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsega.accountBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
